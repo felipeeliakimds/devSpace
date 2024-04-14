dceb32ed2cd988225f7b23e7c904286bbbb3ab9c
felipeeliakimds <felipeeliakim0756@gmail.com>
2024-04-14 08:49:42 -0300

    primeiro commit - teste

---
+++ script.js
@@ -0,0 +1,7 @@
+const logos = document.querySelector(".img")
+
+logos.addEventListener("hover", () =>{
+    const audio = new audio("swoosh-sound-effect-for-transitions.mp3")
+console.log('oi')
+    audio.play();
+})
